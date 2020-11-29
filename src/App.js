91c0f5c33606e47e8d9c655314db55bb067292ca
Julianb18 <julian141718@gmail.com>
2020-11-29 15:40:50 +0100

    finished tutorial with own spin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const quotes = [
+  "In the jungle",
+  "We're no strangers to love",
+  "We could all use a little change",
+  "I'll be back",
+];
 
 function App() {
+  const [counter, setCounter] = useState(2);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>{quotes[counter % quotes.length]}</h2>
+      <button className="btn" onClick={(event) => setCounter(counter + 1)}>
+        Change quote
+      </button>
     </div>
   );
 }
